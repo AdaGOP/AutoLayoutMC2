eaaf98b745023db67485386cbfeb8ef10efc14a2
Handy <handy.kwok@gmail.com>
2020-05-11 14:36:38 +0700

    add ALImagePicker

---
+++ AutoLayout/ALActivityView.swift
@@ -0,0 +1,13 @@
+//
+//  ALActivityView.swift
+//  AutoLayout
+//
+//  Created by Handy Handy on 11/05/20.
+//  Copyright © 2020 khoirunnisa. All rights reserved.
+//
+
+import UIKit
+
+class ALActivityView {
+    
+}
--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imagePicker: ALImagePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.imagePicker = ALImagePicker(presentationController: self, delegate: self)
+        
     }
 
+    @IBAction func uploadDidTap(_ sender: UIButton) {
+        self.imagePicker.present(from: sender)
+    }
+    
+    @IBAction func shareDidTap(_ sender: UIButton) {
+           
+    }
+       
+}
 
+extension ViewController: ALImagePickerDelegate {
+    func didSelect(image: UIImage?) {
+        
+    }
 }
 
