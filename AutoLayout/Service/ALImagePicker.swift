0e0737405d029f917ffe147f3f1d482154be30f8
Handy <handy.kwok@gmail.com>
2020-05-11 15:28:39 +0700

    update commend in ALImagePicker

--- AutoLayout/Service/ALImagePicker.swift
+++ AutoLayout/Service/ALImagePicker.swift
@@ -9,18 +9,19 @@
 import UIKit
 
 // MARK: - ALImagePickerDelegate
-// The protocol was to receive the image from your photo gallery
+/// The protocol was to receive the image from your photo gallery
 protocol ALImagePickerDelegate: class {
     func didSelect(image: UIImage?)
 }
 
 // MARK: - ALImagePicker
-// The ALImagePicker from AutoLayoutImagePicker, "AutoLayout" is the app name.
+/// The ALImagePicker from AutoLayoutImagePicker, "AutoLayout" is the app name.
 class ALImagePicker: NSObject {
     private let pickerController: UIImagePickerController
     private weak var presentationController: UIViewController?
     private weak var delegate: ALImagePickerDelegate?
     
+    /// The init used to create picker controller and set the ALImagePickerDelegate and presentationController with the delegate
     init(presentationController: UIViewController, delegate: ALImagePickerDelegate) {
         self.pickerController = UIImagePickerController()
         
@@ -33,6 +34,7 @@ class ALImagePicker: NSObject {
         self.pickerController.allowsEditing = true
     }
     
+    /// This function is used for generate action in alert, it will based on the source type, so we check the availability in here, if the source type is not available, return nil
     private func action(for type: UIImagePickerController.SourceType, title: String) -> UIAlertAction? {
         guard UIImagePickerController.isSourceTypeAvailable(type) else {
             return nil
@@ -44,6 +46,7 @@ class ALImagePicker: NSObject {
         }
     }
     
+    /// This function will be called from view controller, to trigger the action sheet.
     public func present(from sourceView: UIView) {
         
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -63,6 +66,7 @@ class ALImagePicker: NSObject {
         self.presentationController?.present(alertController, animated: true)
     }
     
+    /// This function will be trigger when some picture is selected.
     private func pickerController(_ controller: UIImagePickerController, didSelect image: UIImage?) {
         controller.dismiss(animated: true, completion: nil)
         
@@ -74,10 +78,12 @@ class ALImagePicker: NSObject {
 // MARK: - UIImagePickerControllerDelegate
 extension ALImagePicker: UIImagePickerControllerDelegate {
     
+    /// This function will be trigger when UIImagePickerController canceled and notify the ALImagePickerDelegate to return nil on the ALImagePickerDelegate.
     public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.pickerController(picker, didSelect: nil)
     }
     
+    /// This function will be trigger when some picture is selected and notify the ALImagePickerDelegate to handle the image.
     public func imagePickerController(_ picker: UIImagePickerController,
                                       didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         guard let image = info[.editedImage] as? UIImage else {
